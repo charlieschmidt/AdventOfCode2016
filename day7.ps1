4f9075f9a417dbc5ece7fe0410fa7386f91b74b1
Charlie Schmidt <charlie.schmidt@equifax.com>
2016-12-07 13:07:43 -0500

    day7

---
+++ day7.ps1
@@ -0,0 +1,82 @@
+
+$Messages = Get-Content (Join-Path $PSScriptRoot day7.input) 
+
+$SupportTLS = 0
+$SupportSSL = 0
+
+foreach ($Message in $Messages)
+{
+    $TLSValid = $null
+    $Outside = $true
+    for ($i = 0; $i -lt $Message.Length - 3; $i++)
+    {
+        $Abba = $Message.Substring($i,4)
+        if ($Abba[0] -eq '[')
+        {
+            $Outside = $false
+            continue
+        }
+        if ($Abba[0] -eq ']')
+        {
+            $Outside = $true
+            continue
+        }
+        if ($Abba[0] -eq $Abba[3] -and $Abba[1] -eq $Abba[2] -and $Abba[0] -ne $Abba[1])
+        {
+            if ($Outside)
+            {
+                $TLSValid = $true
+            }
+            else
+            {
+                $TLSValid = $false
+                $i = $Message.Length
+            }
+        }
+    }
+    if ($TLSValid)
+    {
+        $SupportTLS++
+    }
+
+
+
+
+    $Outside = $true
+    $InsideABAs = @()
+    $OutsideABAs = @()
+    for ($i = 0; $i -lt $Message.Length - 2; $i++)
+    {
+        $ABA = $Message.Substring($i,3)
+        if ($Aba[0] -eq '[')
+        {
+            $Outside = $false
+            continue
+        }
+        if ($Aba[0] -eq ']')
+        {
+            $Outside = $true
+            continue
+        }
+
+        if ($ABA[0] -eq $ABA[2])
+        {
+            if ($Outside)
+            {
+                $OutsideABAs += "{0}{1}{0}" -f $ABA[1],$ABA[0]
+            }
+            else
+            {
+                $InsideABAs += $ABA
+            }
+
+        }
+    }
+    if ($InsideABAs | Where-Object {$OutsideABAs -contains $_})
+    {
+        $SupportSSL++
+    }
+}
+
+Write-Host "Solution 1: $SupportTLS"
+Write-Host "Solution 2: $SupportSSL"
